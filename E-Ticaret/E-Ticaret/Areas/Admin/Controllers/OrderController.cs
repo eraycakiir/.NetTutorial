be9aeca8c7dfcceaef69beba0635edbe8c87bfec
Eray Çakır <99022088+eraycakiir@users.noreply.github.com>
2022-12-18 00:07:26 +0300

    Order

---
+++ E-Ticaret/E-Ticaret/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using E_Ticaret.Data.Repository.IRepository;
+using E_Ticaret.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Ticaret.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderVM OrderVM { get; set; }
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    
+        public IActionResult Index()
+        {
+            var orderList = _unitOfWork.OrderProduct.GetAll(x => x.OrderStatus != "Teslim Edildi");
+            return View(orderList);
+        }
+    }
+}
